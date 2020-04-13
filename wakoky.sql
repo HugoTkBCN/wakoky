7a12e6e2eaa2e86b8d17aafdf2643c9549986aa7
Hugo <hugo1.lachkar@epitech.eu>
2020-04-13 02:24:54 +0200

    empty playlist

--- wakoky.sql
+++ wakoky.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : lun. 13 avr. 2020 à 00:12
+-- Généré le : lun. 13 avr. 2020 à 00:23
 -- Version du serveur :  10.4.12-MariaDB
 -- Version de PHP : 7.4.4
 
@@ -35,24 +35,6 @@ CREATE TABLE `links` (
   `exec_order` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Déchargement des données de la table `links`
---
-
-INSERT INTO `links` (`playlist_id`, `link`, `id`, `name`, `exec_order`) VALUES
-(15, 'rzJBkSBx-Uk', 56, '🎧 XXXTENTACION - Save Me (DareveL Remix) (8D AUDIO) 🎧', 1),
-(15, 'db7_icvfxSE', 68, 'Travis Scott - HIGHEST IN THE ROOM | 8D SOUNDS', 2),
-(15, 'rzJBkSBx-Uk', 71, '🎧 XXXTENTACION - Save Me (DareveL Remix) (8D AUDIO) 🎧', 3),
-(15, 'db7_icvfxSE', 72, 'Travis Scott - HIGHEST IN THE ROOM | 8D SOUNDS', 4),
-(15, 'rzJBkSBx-Uk', 73, '🎧 XXXTENTACION - Save Me (DareveL Remix) (8D AUDIO) 🎧', 5),
-(19, '5BgLoQ1NnJc', 74, 'XXXTENTACION - School Shooters (Official Video) (feat. Lil Wayne)', 1),
-(19, 'db7_icvfxSE', 75, 'Travis Scott - HIGHEST IN THE ROOM | 8D SOUNDS', 2),
-(19, 'rzJBkSBx-Uk', 76, '🎧 XXXTENTACION - Save Me (DareveL Remix) (8D AUDIO) 🎧', 3),
-(19, 'rzJBkSBx-Uk', 77, '🎧 XXXTENTACION - Save Me (DareveL Remix) (8D AUDIO) 🎧', 4),
-(19, 'db7_icvfxSE', 78, 'Travis Scott - HIGHEST IN THE ROOM | 8D SOUNDS', 5),
-(19, 'db7_icvfxSE', 79, 'Travis Scott - HIGHEST IN THE ROOM | 8D SOUNDS', 6),
-(20, '5BgLoQ1NnJc', 80, 'XXXTENTACION - School Shooters (Official Video) (feat. Lil Wayne)', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -64,25 +46,6 @@ CREATE TABLE `playlists` (
   `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `playlists`
---
-
-INSERT INTO `playlists` (`id`, `name`, `user_id`) VALUES
-(14, 'ma playliste de bg', 8),
-(15, 'myPlaylist', 1),
-(19, 'more bitch', 1),
-(20, 'la', 1),
-(22, 'f', 1),
-(23, '1', 1),
-(24, '2', 1),
-(25, '5', 1),
-(26, 'ls', 1),
-(27, '3', 1),
-(30, 'rap', 1),
-(31, '53', 1),
-(32, 'gh', 1);
 
 -- --------------------------------------------------------
 
@@ -96,15 +59,6 @@ CREATE TABLE `users` (
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(1, 'hugo', 'hugolachkar@gmail.com', '3e031b4cec984b466a226d7dc5206859'),
-(7, 'dana', 'test@gmail.com', '098f6bcd4621d373cade4e832627b4f6'),
-(8, 'cycy', 'cycy@gmail.com', '202cb962ac59075b964b07152d234b70');
 
 --
 -- Index pour les tables déchargées
@@ -136,19 +90,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `links`
 --
 ALTER TABLE `links`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `playlists`
 --
 ALTER TABLE `playlists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
