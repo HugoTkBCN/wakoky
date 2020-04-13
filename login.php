df0c122a4ba9574ca75fb47b6e620e8ec313c136
Hugo <hugo1.lachkar@epitech.eu>
2020-04-13 04:10:31 +0200

    add css for login and register

--- login.php
+++ login.php
@@ -1,39 +1,34 @@
 <?php include('server.php') ?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-	<title>Registration system PHP and MySQL</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Wakoky Sign in</title>
+	<link rel="stylesheet" type="text/css" href="login.css">
+	<link rel="icon" type="image/ico" href="logo_zoom.png" />
 </head>
 
 <body>
+	<div class="login_form">
 
-	<div>
-		<h2>Login</h2>
+		<setion class="login-wrapper">
+			<div class="logo">
+				<img src="logo.png" alt=""></a>
+			</div>
+
+			<form id="login" method="post" action="login.php">
+				<?php include('errors.php'); ?>
+				<label for="username">User Name</label>
+				<input required name="username" type="text" autocapitalize="off" autocorrect="off" />
+
+				<label for="password">Password</label>
+				<input class="password" required name="password" type="password" />
+				<button type="submit" name="login_user">Sign In</button>
+				<div class="sign_up"><a href="register.php">You don't have an account ?  Sign Up Here</a></div>
+			</form>
+			</section>
 	</div>
-
-	<form method="post" action="login.php">
-
-		<?php include('errors.php'); ?>
-
-		<div>
-			<label>Username</label>
-			<input type="text" name="username">
-		</div>
-		<div>
-			<label>Password</label>
-			<input type="password" name="password">
-		</div>
-		<div>
-			<button type="submit" name="login_user">Login</button>
-		</div>
-		<p>
-			Not yet a member? <a href="register.php">Sign up</a>
-		</p>
-	</form>
-
-
 </body>
 
 </html>
--- register.php
+++ register.php
@@ -1,44 +1,40 @@
 <?php include('server.php') ?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-	<title>Registration system PHP and MySQL</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Wakoky Sign In</title>
+	<link rel="stylesheet" type="text/css" href="register.css">
+	<link rel="icon" type="image/ico" href="logo_zoom.png" />
 </head>
 
 <body>
-	<div>
-		<h2>Register</h2>
+	<div class="register_form">
+
+		<setion class="register-wrapper">
+			<div class="logo">
+				<img src="logo.png" alt=""></a>
+			</div>
+
+			<form id="register" method="post" action="register.php">
+				<?php include('errors.php'); ?>
+				<label for="username">User Name</label>
+				<input required name="username" type="text" autocapitalize="off" autocorrect="off" />
+
+				<label for="email">Email</label>
+				<input required name="email" type="text" autocapitalize="off" autocorrect="off" />
+
+				<label for="password">Password</label>
+				<input class="password" required name="password_1" type="password" />
+				<label for="password">Confirm Password</label>
+				<input class="password" required name="password_2" type="password" />
+				<div>
+				<button type="submit" name="reg_user">Sign Up</button>
+				<div class="sign_in"><a href="login.php">Already a member ?  Sign In Here</a></div>
+			</form>
+			</section>
 	</div>
-
-	<form method="post" action="register.php">
-
-		<?php include('errors.php'); ?>
-
-		<div>
-			<label>Username</label>
-			<input type="text" name="username" value="<?php echo $username; ?>">
-		</div>
-		<div>
-			<label>Email</label>
-			<input type="email" name="email" value="<?php echo $email; ?>">
-		</div>
-		<div>
-			<label>Password</label>
-			<input type="password" name="password_1">
-		</div>
-		<div>
-			<label>Confirm password</label>
-			<input type="password" name="password_2">
-		</div>
-		<div>
-			<button type="submit" name="reg_user">Register</button>
-		</div>
-		<p>
-			Already a member? <a href="login.php">Sign in</a>
-		</p>
-	</form>
 </body>
 
 </html>
