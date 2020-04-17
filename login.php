7b0d78bd2309748957a18e52db4c2297744bd2ef
Hugo <hugo1.lachkar@epitech.eu>
2020-04-17 18:17:25 +0200

    test amazon pub

--- login.php
+++ login.php
@@ -52,6 +52,7 @@
 				</section>
 		</div>
 	</div>
+	<iframe src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=7&l=ez&f=ifr&linkID=85a35c287c6ab37af90cf827cdccda03&t=wakoky-20&tracking_id=wakoky-20" width="468" height="40" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
 </body>
 
 </html>
