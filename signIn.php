1da86b0647816d4ba8a0f6a832a31f9d95234c3a
Hugo <hugo1.lachkar@epitech.eu>
2020-04-20 18:57:34 +0200

    remove amazon pubs

--- signIn.php
+++ signIn.php
@@ -53,7 +53,6 @@
 				</form>
 				</section>
 		</div>
-		<iframe src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=13&l=ez&f=ifr&linkID=938f3a87bfe6db967bfe39c63b5246b7&t=wakoky-20&tracking_id=wakoky-20" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
 	</div>
 </body>
 
